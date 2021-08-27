f475662ed30f9235bb102ae4baa7a99670666e62
Chris Mugavero <cjmugs@gmail.com>
2021-08-26 20:24:04 -0500

    updates

--- buttonpress.py
+++ buttonpress.py
@@ -12,9 +12,8 @@ GPIO.setup(20, GPIO.OUT)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def button_callback(channel):
-    blinkOnce(20)
     print('light is on')
 
 GPIO.add_event_detect(6,GPIO.RISING,callback=button_callback) 
-message = input("Press enter to quit\n\n") 
+message = input("Press enter to quit\n\n")
 GPIO.cleanup()
