c32c8f268f61431ddf7bd6a72906f72acdd0737e
Chris Mugavero <cjmugs@gmail.com>
2021-08-26 20:22:25 -0500

    updates

--- buttonpress.py
+++ buttonpress.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import webbrowser
+from testlight import blinkOnce
 
 # initialize the GPIO
 GPIO.setwarnings(False)
@@ -11,8 +11,8 @@ GPIO.setup(27, GPIO.OUT)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def button_callback(channel):
-    webbrowser.open('www.google.com', new=1)
-    print('page has opened')
+    blinkOnce(20)
+    print('light is on')
 
 GPIO.add_event_detect(6,GPIO.RISING,callback=button_callback) 
 message = input("Press enter to quit\n\n") 
